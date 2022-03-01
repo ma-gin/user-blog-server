f451a62481b557a78f8cf8fa01e7c18bb5f71661
Matt Ginter <mattginter@gmail.com>
2022-03-01 23:41:10 +0100

    post request working

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,27 +1,28 @@
-// GET /authors => returns the list of authors
-// GET /authors/123 => returns a single author
-// POST /authors => create a new author
-// PUT /authors/123 => edit the author with the given id
-// DELETE /authors/123 => delete the author with the given id
 import fs from "fs"
 import express from "express"
 import { fileURLToPath } from "url"
-import { dirname, join } from "path"
+import { join, dirname } from "path"
+import uniqid from "uniqid"
 
-console.log(import.meta.url)
-
-const currentFilePath = fileURLToPath(import.meta.url)
-const parentFolderPath = dirname(currentFilePath)
-const authorsJSONPath = join(parentFolderPath, "authors.json")
+const authorsJSONPath = join(
+  dirname(fileURLToPath(import.meta.url)),
+  "authors.json"
+)
 
 const authorsRouter = express.Router()
 
-authorsRouter.post("/", (request, response) => {
+authorsRouter.post("/", (req, response) => {
+  console.log("body:", req.body)
+  const newAuthor = { ...req.body, createdAt: new Date(), id: uniqid() }
+  console.log(newAuthor)
+  //   const authorsArray = JSON.parse(req.body)
   response.send({ message: "hi mom" })
 })
-authorsRouter.get("/", (request, response) => {
+
+authorsRouter.get("/", (request, resp) => {
   const fileContent = fs.readFileSync(authorsJSONPath)
   const authorsArray = JSON.parse(fileContent)
-  response.send(authorsArray)
+  resp.send(authorsArray)
 })
+
 export default authorsRouter
