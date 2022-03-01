658218f67ec8033076a3f0468c6de49bda2a0f42
Matt Ginter <mattginter@gmail.com>
2022-03-01 22:03:29 +0100

    add express and server.js

---
+++ src/server.js
@@ -0,0 +1,9 @@
+console.log("hi world")
+import express from "express"
+
+const server = express()
+const port = 3003
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}.`)
+})
